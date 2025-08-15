0221cb402eaf133830c62a013c1627279d620fa1
usmanjoy <usmanjoy89@gmail.com>
2025-08-15 16:45:18 +0600

    modifed conftest.py for adding html report generation in local pytest runs

--- conftest.py
+++ conftest.py
@@ -1,4 +1,12 @@
 import pytest
+import os
+
+def pytest_configure(config):
+    """Configure pytest-html to generate reports automatically"""
+    if not config.option.htmlpath:
+        config.option.htmlpath = "report.html"
+    if not config.option.self_contained_html:
+        config.option.self_contained_html = True
 
 @pytest.fixture(scope="function")
 def page_browser(page):
